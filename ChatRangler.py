7bea105c8adca459064699135a993a11ee0219da
Isaac Monroe <isaac@95visual.com>
2025-03-03 18:18:38 -0800

    fixed double thread start in rando

--- ChatRangler.py
+++ ChatRangler.py
@@ -356,7 +356,8 @@ def debugger_chat():
             if not chat_active:
                 print("rando will not do anything until start is run")
             rando_active = True
-            rando_thread.start()
+            if not rando_thread.is_alive:
+                rando_thread.start()
             print("Rando has started. run for your lives")
             
         
